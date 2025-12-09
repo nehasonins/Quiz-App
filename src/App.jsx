18dbd2aab514bf12bca7b89328075cf2188d336f
NEHA\nehas <nehasoni78500@gmail.com>
2025-12-09 21:08:12 +0530

    Rename quiz.jsx to Quiz.jsx and fix imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Quiz from './component/quiz'
+import Quiz from './component/Quiz'
 
 function App() {
   const [count, setCount] = useState(0)
